7f78df2084604882d761b2b118afcd7505366737
riggins-1 <ra.higgins@yahoo.com>
2026-08-07 19:57:29 +0000

    Add ransomware group analysis

---
+++ sql/02_top_ransomware_groups.sql
@@ -0,0 +1,11 @@
+SELECT
+    claim_gang AS ransomware_group,
+    COUNT(*) AS total_attacks
+FROM
+    `ransomware_intel.attacks`
+WHERE
+    claim_gang IS NOT NULL
+GROUP BY
+    ransomware_group
+ORDER BY
+    total_attacks DESC;
